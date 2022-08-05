b7f8472c6090e0a2f6baa3bbfff725dce43c40e4
aman raj <amanraj78622@gmail.com>
2022-08-06 02:05:51 +0530

    hello triyam

--- src/day2/application.java
+++ src/day2/application.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 package day2;
 
 public class application {
 	public static void main(String[] args) {
-	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("whats your name");
+		String name = sc.nextString();
+		System.out.println(name);
 	}
 }
